74510438244ed63ffd349fc79c1187f716be4827
mjtamayo <mariajose.tamayo@gmail.com>
2016-11-03 20:01:54 +0000

    removed old code

--- public/main.js
+++ public/main.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
   var socket = io();
+  
+  // variables for selecting elements from the DOM
   var input = $('#message-input');
   var messages = $('#messages');
-  
-  
   var usernameForm = $('#nicknameForm');
   var userButton = $('.userButton')
   var messagesWrap = $('#messagesWrap')
   var nicknameWrap = $('.nicknameWrap')
   var usernameInput= $('#usernameInput')
   var usersWrap = $('#usersWrap')
-  var disconnectButton = $('#disconnectBtn')
   var userConnected = $('#status')
   
+  // hiding message box and users list when user enters their nickname
   messagesWrap.hide();
   usersWrap.hide();
-  disconnectButton.hide();
   
+  // variables that will be used to store user's information
   var username = "";
   var usersArray = []
-  // var idArray = []
   var user = {}
-  var id
   
+  ///// Functions that will be used to manipulate the DOM ////
   var addMessage = function(messageData) { // function to append the data and username to the DOM
     messages.append('<b>' + messageData.username + '</b><div>' + messageData.message + '</div>');
   };
@@ -33,33 +32,37 @@ $(document).ready(function() {
   }
   
   var addToUserList = function (nickname) {
-    //new
     user.id = socket.io.engine.id
     user.nickname = nickname
     socket.emit('app_user', user)
-    // usersWrap.append('<li>' + user.nickname + '</li>')
-    //new
-    //old
-    // usersWrap.append('<li>' + user + '</li>')
-    //old
   }
   
   var addUserList = function(users) {
-    //new
     console.log("WHAT IS USERS? ", users)
     usersWrap.html()
     usersWrap.empty()
     users.map(user =>  usersWrap.append('<li>' + ((user.nickname) ? user.nickname : user.id) + '</li>'))
-    //new
-    //old
-    // usersWrap.append('<li>' + users + '</li>')
-    //old
   }
   
-  // var removeUser = function(user) {
-  //   usersArray.splice(usersArray.indexOf(user), 1);
-  // }
+  usernameForm.submit(function(event){
+    event.preventDefault();
+    nicknameWrap.hide();
+    messagesWrap.show();
+    username = usernameInput.val()
+    
+    user['nickname'] = username
+    addToUserList(user.nickname)
+    console.log(user)
+    
+    usersWrap.show();
+    console.log("The user is: ", user)
+    userStatus(user.nickname)
+    usersArray.push(user);
+    console.log(usersArray)
+  });
   
+  ///// Event Listners that will communicate with the server //////
+
   input.on('keydown', function(event) { // When user presses enter, event if triggered
     if (event.keyCode != 13) {
       return;
@@ -73,87 +76,11 @@ $(document).ready(function() {
     input.val(''); 
   });
   
-  usernameForm.submit(function(event){
-    event.preventDefault();
-    nicknameWrap.hide();
-    messagesWrap.show();
-    username = usernameInput.val()
-    
-    user['nickname'] = username
-    //new
-    addToUserList(user.nickname)
-    //new
-    //old
-    // addUserList(user.nickname)
-    //old
-    console.log(user)
-    
-    usersWrap.show();
-    disconnectButton.show();
-    // socket.connect()
-    console.log("The user is: ", user)
-    userStatus(user.nickname)
-    usersArray.push(user);
-    console.log(usersArray)
-    //old
-    // socket.emit('users', user);
-    //old
-    
-    // socket.emit('disconnect', usersArray);
-    // removeUser(usersArray)
-    // user.on('disconnect', function(){
-    //   users.splice(users.indexOf(user), 1);
-    // });
-    
-  });
-  
-  
-  
-  // disconnectButton.click(function(event){
-  //   event.preventDefault()
-  //   socket.disconnect();
-  //   // socket.emit('disconnect', username)
-  //   // removeUser(username)
-  // })
-  
-  //new
   socket.on('all_users', function (allUsers) {
     console.log("RECEIVING ALL USERS: ", allUsers)
     usersArray = allUsers
     addUserList(usersArray)
   })
-  //new
+  
   socket.on('message', addMessage); // adds message to the div every time the server send you a message
-  //old
-  // socket.on('users', addUserList);
-  //old
-  socket.on('connect', function(){
-    console.log('user is connected')
-    // id = socket.io.engine.id
-    // console.log(id)
-    // idArray.push(id)
-    //old
-    // user = {
-    //   id:socket.io.engine.id,
-    // }  
-    // socket.emit('app_user', user)
-    //old
-    
-    // socket.on('disconnect', function(){
-    //   console.log(user.nickname + ' ' +  "went offline")
-    //   removeUser(user)
-    //   console.log(usersArray)
-    // })
-  })
-  
-
-  
-  
-  // socket.on('connect', function (user) {
-  //   addUserList(usersArray)
-  //   user.on('disconnect', function(){
-  //     usersArray.splice(usersArray.indexOf(user), 1);
-  //   });
-  // });
-  
 });
--- server.js
+++ server.js
@@ -16,28 +16,17 @@ io.on('connection', function (socket) {
     console.log('Client connected');
     
     socket.on('app_user', function(user){
-        //new
         socket.user = user
         app_users.push(user)
         console.log(app_users)
         console.log("ON THE SERVER, ALL USERS ARE: ", app_users)
         io.sockets.emit('all_users', app_users) //this broadcasts to ALL sockets the all users array
-        //new
     })
 
     socket.on('message', function(messageData) { // receives messageData object from main.js
         console.log('Received message:', messageData.message);
         socket.broadcast.emit('message', messageData); // sends the message to all the users of the chat
     });
-    
-    // socket.on('users', function(user){
-    //     //new
-    //     socket.broadcast.emit('users', app_users)
-    //     //new
-    //     //old
-    // //   socket.broadcast.emit('users', user.nickname)
-    //   //old
-    // })
     
     socket.on('disconnect', function(user){
         console.log('user is disconnected')
